4b676f427fe3ecc0fcd1f55e5598189f921ca58e
fryntiz <tecnico@fryntiz.es>
2017-05-13 16:53:47 +0200

    Creada base para info

---
+++ info.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
+
+############################
+##   Constantes Colores   ##
+############################
+amarillo="\033[1;33m"
+AZUL="\033[1;34m"
+blanco="\033[1;37m"
+cyan="\033[1;36m"
+gris="\033[0;37m"
+magenta="\033[1;35m"
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+LIMPIAR="\033[1;00m"
+
+#############################
+##   Variables Generales   ##
+#############################
+x=$VERDE
+y=$ROJO
+
+
+
+#############################
+##         BÁSICOS         ##
+#############################
+echo -e "$AZUL Comandos básicos de BASH"
+echo ""
+echo -e "$x !!$y Repetir último comando"
+echo -e "$x echo \$SHELL$y Muestra en la pantalla la SHELL que estás usando"
+echo -e "$x echo \$BASH_VERSION$y Muestra la versión de BASH"
+echo -e "$x bash$y expecifica que use bash"
+echo -e "$x whereis bash$y Busca donde está guardado bash en el sistema"
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#FINAL --> Limpia color:
+echo -e "$LIMPIAR"
