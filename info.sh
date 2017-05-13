5f0692376b53e1d414d4e901c72b21b865b04100
fryntiz <tecnico@fryntiz.es>
2017-05-13 17:02:00 +0200

    Agregados nuevos comandos

--- info.sh
+++ info.sh
@@ -32,21 +32,21 @@ y=$ROJO
 #############################
 ##         BÁSICOS         ##
 #############################
-echo -e "$AZUL Comandos básicos de BASH"
+echo -e "$AZUL Comandos básicos de BASH$LIMPIAR"
 echo ""
 echo -e "$x !!$y Repetir último comando"
 echo -e "$x echo \$SHELL$y Muestra en la pantalla la SHELL que estás usando"
 echo -e "$x echo \$BASH_VERSION$y Muestra la versión de BASH"
 echo -e "$x bash$y expecifica que use bash"
 echo -e "$x whereis bash$y Busca donde está guardado bash en el sistema"
-echo -e "$x  $y "
-echo -e "$x  $y "
-echo -e "$x  $y "
-echo -e "$x  $y "
-echo -e "$x  $y "
-echo -e "$x  $y "
-echo -e "$x  $y "
-echo -e "$x  $y "
+echo -e "$x pwd$y Devuelve la ruta del directorio actual o directorio activo"
+echo -e "$x cd [nombre_directorio]$y Cambia el directorio actual por el pasado como parámetro."
+echo -e "$x mkdir [directorio]$y Crea un nuevo directorio llamado directorio"
+echo -e "$x rmdir [directorio]$y Elimina el directorio llamado directorio si está vacío"
+echo -e "$x logout/exit$y Finaliza la sesión"
+echo -e "$x man [comando]$y Muestra el manual/la documentación del comando pasado como parámetro."
+echo -e "$x [comando] 2>$y Redirige a un archivo o dispositivo cuando se produce un error"
+echo -e "$x [comando] 2>>$y Para añadir en vez de reemplazar se usa “2>>”"
 echo -e "$x  $y "
 echo -e "$x  $y "
 echo -e "$x  $y "
@@ -78,4 +78,6 @@ echo -e "$x  $y "
 
 #FINAL --> Limpia color:
 echo -e "$LIMPIAR"
+
+#Indicar fin del script correcto
 exit 0
