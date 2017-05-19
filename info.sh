b69938c1f7ffeb2292fc1617eae766a38d98c53c
fryntiz <tecnico@fryntiz.es>
2017-05-19 11:38:10 +0200

    TAR

--- info.sh
+++ info.sh
@@ -191,8 +191,18 @@ find ./dir1/ ./dir2 / -type f  Buscar en varios directorios a la vez
 
 echo ""
 echo ""
-echo -e "$x $y"
+echo -e "$x TAR $y"
 echo -e '
+-v Vebose, visualiza por pantalla las operaciones que va realizando
+-c Create, crea un archivo .tar
+-f File, indica que se le dará un nombre al fichero .tar
+-x Extract, extrae los archivos
+-t Lista el contenido
+-z Realiza el empaquetado y la compresión en un solo paso, se puede utilizar la extensión tgz que es equivalente a tar.gz
+-C Especifica el destino de extracción: tar -xvzf fichero.tgz -C backup/
+
+tar -cvzf comprimir.tgz ficheros..  Comprime y empaqueta todos los ficheros pasados
+tar -xvzf fichero.tgz  Descomprime los ficheros empaquetados y comprimidos
 
 '
 
