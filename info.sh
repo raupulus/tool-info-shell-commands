2eece1fd93dcfa0bbefdbd1625ccb418b2a82863
fryntiz <tecnico@fryntiz.es>
2017-05-19 11:20:14 +0200

    Condicionales para archivos

--- info.sh
+++ info.sh
@@ -79,8 +79,27 @@ echo -e "$x $y "
 
 
 echo -e "$x crontab -e$y minutos	hora	dia	mes	dia de la semana	usuario		comando"
-echo -e "$x $y "
-echo -e "$x $y "
+echo -e "$x Expresiones condicionales para archivos"
+echo '
+-r <fichero> Es verdadero si existe y se puede leer
+-w <fichero> Es verdadero si existe y se puede escribir
+-x <fichero> Es verdadero si existe y es ejecutable
+-f <fichero> Es verdadero si solo existe
+-d <directorio> Es verdadero si es un directorio
+-s <fichero> Es verdadero si existe y tiene un tamaño superior a 0
+'
+
+
+echo -e "$x Expresiones condicionales para Cadenas de Carácteres"
+echo -e '
+
+'
+
+
+
+
+
+
 echo -e "$x $y "
 echo -e "$x $y "
 echo -e "$x $y "
