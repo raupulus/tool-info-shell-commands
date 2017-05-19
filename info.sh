da57c97ed251bb65e60e8a8a6b885c265c148410
fryntiz <tecnico@fryntiz.es>
2017-05-19 11:40:22 +0200

    Reestructurado

--- info.sh
+++ info.sh
@@ -218,7 +218,42 @@ Asignar IP, mascara y broadcast Ifconfig eth0 192.168.9.2 netmask 255.255.255.0 
 
 echo ""
 echo ""
+echo -e "$x RED CON IFCONFIG $y"
+echo -e '
 
+'
+
+
+echo ""
+echo ""
+echo -e "$x RED CON IFCONFIG $y"
+echo -e '
+
+'
+
+
+echo ""
+echo ""
+echo -e "$x RED CON IFCONFIG $y"
+echo -e '
+
+'
+
+
+echo ""
+echo ""
+echo -e "$x RED CON IFCONFIG $y"
+echo -e '
+
+'
+
+
+echo ""
+echo ""
+echo -e "$x RED CON IFCONFIG $y"
+echo -e '
+
+'
 
 #FINAL --> Limpia color:
 echo -e "$LIMPIAR"
