b66e9f4aff6587c4fc1438a4be267a3c0a06b623
fryntiz <tecnico@fryntiz.es>
2017-05-24 21:11:58 +0200

    Creada función para mostrar información de cron

--- cron.sh
+++ cron.sh
@@ -7,3 +7,17 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+function cron() {
+	echo ""
+	echo -e "$x crontab -e$y minutos	hora	dia	mes	dia de la semana	usuario		comando"
+	echo -e "$x Expresiones condicionales para archivos$y"
+	echo '
+	-r <fichero> Es verdadero si existe y se puede leer
+	-w <fichero> Es verdadero si existe y se puede escribir
+	-x <fichero> Es verdadero si existe y es ejecutable
+	-f <fichero> Es verdadero si solo existe
+	-d <directorio> Es verdadero si es un directorio
+	-s <fichero> Es verdadero si existe y tiene un tamaño superior a 0
+	'
+}
