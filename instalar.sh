1818649d7ff36160f0e7003eec3a365e7f2d0c39
fryntiz <tecnico@fryntiz.es>
2017-05-25 00:25:33 +0200

    Planteadas las dos vias posibles de instalación

--- instalar.sh
+++ instalar.sh
@@ -7,3 +7,18 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+#El instalador está pendiente de ser revisado
+#No se garantiza su funcionamiento actualmente
+
+####FORMA 1 --> Extrayendo desde GIT con "git clone"
+# mkdir /tmp/amigoScript
+# cd /tmp/amigoScript
+# COMPROBAR QUE ESTOY EN /tmp/amigoScript
+# git clone https://github.com/fryntiz/infoshell
+
+####FORMA 2 --> Autocopiándose dentro de /usr
+# mkdir /usr/¿?????
+# cp ./* /usr/???????????
+# crear archivo desktop
+# crear archivo para man (manual)
