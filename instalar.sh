6f83bdf5174a795dd752e53669919b26a802fbf7
fryntiz <tecnico@fryntiz.es>
2017-05-25 01:13:56 +0200

    Mejorado instalador

--- instalar.sh
+++ instalar.sh
@@ -19,20 +19,30 @@ echo "Si quieres puedes descomentar las líneas o instalar este programa manualm
 # sudo apt-get update
 # sudo apt-get install xterm git??????????
 
-####Pasos para instalar --> Autocopiándose dentro de /usr
+####Crear directorio de instalación
 # sudo mkdir /usr/share/amigo
 # sudo cp ./* /usr/share/amigo
-# crear archivo desktop
+
+###Crear archivo desktop
 # sudo cp amigo.desktop /usr/share/applications/
 
 ###Crear archivo para man (manual)
 
+###Copiar icono
+# sudo cp amigo.svg /usr/share/icons/
+
+
 ###Por último crear lanzador en /usr/bin
 # sudo touch /usr/bin/amigo
-# echo "#!/bin/bash"
-# echo "cd /usr/share/amigo" > /usr/bin/amigo
-# echo "sh amigo.sh" >> /usr/bin/amigo
-# echo "exit 0" >> /usr/bin/amigo
+# sudo echo "#!/bin/bash"
+# sudo echo "cd /usr/share/amigo" > /usr/bin/amigo
+# sudo echo "sh amigo.sh" >> /usr/bin/amigo
+# sudo echo "exit 0" >> /usr/bin/amigo
+
+###Reparar permisos
+# sudo chmod 755 /usr/bin/amigo
+# sudo chmod 755 /usr/share/icons/amigo.svg
+# sudo chmod 755 /usr/share/applications/amigo.desktop
 
 
 ###Comprobar que se han creado los directorios y archivos anteriores, en caso contrario salir con error
