59d95c0ce03ae433ac22c98cc35a89ac69810bb6
fryntiz <tecnico@fryntiz.es>
2017-05-24 23:26:12 +0200

    Llamada a la función otros

--- amigo.sh
+++ amigo.sh
@@ -37,56 +37,6 @@ function ayuda() {
 
 echo ""
 echo ""
-echo -e "$x Expresiones condicionales para Cadenas de Carácteres$y"
-echo -e '
--z “cadena” Verdadero si la longitud es 0
--n “cadena” Verdadero si la longitud no es 0
-“cadena1” = “cadena2” Verdadero si las cadenas son iguales
-“cadena1” != “cadena2” Verdadero si las cadenas no son iguales
-'
-
-echo ""
-echo ""
-echo -e "$x Expresiones condicionales para Operaciones Aritméticas$y"
-echo -e '
--eq Igual, viene de “equal”
--ne Distinto, viene de “not equal”
--gt Mayor que, viene de “bigger than”
--lt Menor que, viene de “less than”
--ge Mayor o igual, viene de “bigger equal”
--le Menor o igual, viene de “less equal”
-'
-
-echo ""
-echo ""
-echo -e "$x Leer Archivo Línea por línea$y"
-echo -e '
-while read line
-do
-	echo “$line”
-done <$1
-'
-
-echo ""
-echo ""
-echo -e "$x CAMBIAR IFS$y"
-echo -e '
-La variable IFS contiene el valor del campo por el cual separará un bucle como el for
-
-IFSanterior=$IFS
-IFS=$'\n'
-
-for i in `cat $1`
-do
-	echo $i
-done
-IFS=$FSanterior #Reestablece la variable IFS a su valor original
-'
-
-
-
-echo ""
-echo ""
 echo -e "$x  $y"
 echo -e '
 
@@ -116,6 +66,8 @@ Xusuarios
 source ./cron.sh
 Xcron
 
+source ./otros.sh
+Xotros
 
 #FINAL --> Limpia color:
 echo -e "$LIMPIAR"
