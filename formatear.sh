a1ea71723d0b12b286a2b895ad417e3bb4472505
fryntiz <tecnico@fryntiz.es>
2017-05-24 21:50:28 +0200

    Creada función para mostrar información sobre formatear

--- formatear.sh
+++ formatear.sh
@@ -7,3 +7,25 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+function Xformatear() {
+	echo ""
+	echo -e "$x FDISK $y"
+	echo -e '
+	fdisk /dev/sdb
+
+	m Muestra la ayuda
+	p Muestra la tabla de particiones
+	n Crea una nueva partición
+	d Elimina una partición
+	q Salir sin guardar los cambios
+	w Guardar la partición, los cambios realizados y salir
+	'
+
+	echo ""
+	echo -e "$x FORMATEAR EXT4 $y"
+	echo -e '
+	Partición en formato ext4
+	mkfs -t ext4 /dev/sdb3
+	'
+}
