2faf7d1453334b09f3b8085d0486c0218854f28e
fryntiz <tecnico@fryntiz.es>
2017-05-25 00:05:52 +0200

    Añadida información de uso

--- amigo.sh
+++ amigo.sh
@@ -29,38 +29,52 @@ y=$AMARILLO
 
 echo -e "$AZUL Comandos básicos de BASH$LIMPIAR"
 
-#function ayuda() {
-	#Información sobre el uso del comando
+#Información sobre el uso del comando
+function Xayuda() {
+	echo -e "$AZUL FORMA DE USAR ESTE COMANDO$LIMPIAR"
+	echo -e "$ROJO $0$VERDE información$LIMPIAR"
 	echo ""
-#}
+	echo -e "$magenta Los tipos de información posible son:$LIMPIAR"
+	echo -e "$VERDE  > info$AMARILLO: Información general$LIMPIAR"
+	echo -e "$VERDE  > grep$AMARILLO: Filtrar mediante el comando grep$LIMPIAR"
+	echo -e "$VERDE  > comprimir$AMARILLO: Comprimir y empaquetar archivos y directorios$LIMPIAR"
+	echo -e "$VERDE  > red$AMARILLO: Configuración de Red estática y por DHCP$LIMPIAR"
+	echo -e "$VERDE  > buscar$AMARILLO: Sintaxis básica para el comando find$LIMPIAR"
+	echo -e "$VERDE  > formatear$AMARILLO: Comandos para particionar discos y formatearlos$LIMPIAR"
+	echo -e "$VERDE  > usuarios$AMARILLO: Gestión de usuarios y grupos$LIMPIAR"
+	echo -e "$VERDE  > cron$AMARILLO: Programar tareas mediante crontab -e$LIMPIAR"
+	echo -e "$VERDE  > otros$AMARILLO: Otros comandos y opciones útiles$LIMPIAR"
+}
 
-if [ $# -eq 0 ] -o [ $# -gt 1 ]; then
-	#Xayuda
-elif [ $1 -eq "info"]; then
+#Comprobando parámetro de entrada
+if [ $# -eq 0 ] || [ $# -gt 1 ]; then
+	echo "cond1"
+	Xayuda
+elif [ $1 = "info" ]; then
 	source ./info.sh
 	Xinfo
-elif [ $1 -eq "grep"]; then
+elif [ $1 = "grep" ]; then
 	source ./grep.sh
 	Xgrep
-elif [ $1 -eq "comprimir"]; then
+elif [ $1 = "comprimir" ]; then
 	source ./comprimir.sh
 	Xcomprimir
-elif [ $1 -eq "red"]; then
+elif [ $1 = "red" ]; then
 	source ./red.sh
 	Xred
-elif [ $1 -eq "buscar"]; then
+elif [ $1 = "buscar" ]; then
 	source ./buscar.sh
 	Xfind
-elif [ $1 -eq "formatear"]; then
+elif [ $1 = "formatear" ]; then
 	source ./formatear.sh
 	Xformatear
-elif [ $1 -eq "usuarios"]; then
+elif [ $1 = "usuarios" ]; then
 	source ./usuarios.sh
 	Xusuarios
-elif [ $1 -eq "cron"]; then
+elif [ $1 = "cron" ]; then
 	source ./cron.sh
 	Xcron
-elif [ $1 -eq "otros"]; then
+elif [ $1 = "otros" ]; then
 	source ./otros.sh
 	Xotros
 else
