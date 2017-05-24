4c9d55f17592ccf3a6d4e9fb7338058168d4929c
fryntiz <tecnico@fryntiz.es>
2017-05-25 00:59:13 +0200

    Replanteados pasos para crear instalador

--- instalar.sh
+++ instalar.sh
@@ -11,20 +11,29 @@
 #El instalador está pendiente de ser revisado
 #No se garantiza su funcionamiento actualmente
 
+echo "El instalador aún no se ha verificado completamente"
+echo "Si quieres puedes descomentar las líneas o instalar este programa manualmente"
 
 ####Antes de nada:
+# Comprobar que existen las dependencias, si no existen realizar la instalación
 # sudo apt-get update
 # sudo apt-get install xterm git??????????
 
-
-####FORMA 1 --> Extrayendo desde GIT con "git clone"
-# mkdir /tmp/amigoScript
-# cd /tmp/amigoScript
-# COMPROBAR QUE ESTOY EN /tmp/amigoScript
-# git clone https://github.com/fryntiz/infoshell
-
-####FORMA 2 --> Autocopiándose dentro de /usr
-# mkdir /usr/¿?????
-# cp ./* /usr/???????????
+####Pasos para instalar --> Autocopiándose dentro de /usr
+# sudo mkdir /usr/share/amigo
+# sudo cp ./* /usr/share/amigo
 # crear archivo desktop
-# crear archivo para man (manual)
+# sudo cp amigo.desktop /usr/share/applications/
+
+###Crear archivo para man (manual)
+
+###Por último crear lanzador en /usr/bin
+# sudo touch /usr/bin/amigo
+# echo "#!/bin/bash"
+# echo "cd /usr/share/amigo" > /usr/bin/amigo
+# echo "sh amigo.sh" >> /usr/bin/amigo
+# echo "exit 0" >> /usr/bin/amigo
+
+
+###Comprobar que se han creado los directorios y archivos anteriores, en caso contrario salir con error
+exit 0
