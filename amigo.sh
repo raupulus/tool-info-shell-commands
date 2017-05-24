791c913a685bc1586c58d674a3e845d56797d73a
fryntiz <tecnico@fryntiz.es>
2017-05-24 21:12:21 +0200

    quitada la parte de cron que ahora es externa

--- amigo.sh
+++ amigo.sh
@@ -81,19 +81,6 @@ function basicos() {
 	echo -e "$x $y "
 }
 
-echo ""
-echo ""
-echo -e "$x crontab -e$y minutos	hora	dia	mes	dia de la semana	usuario		comando"
-echo -e "$x Expresiones condicionales para archivos$y"
-echo '
--r <fichero> Es verdadero si existe y se puede leer
--w <fichero> Es verdadero si existe y se puede escribir
--x <fichero> Es verdadero si existe y es ejecutable
--f <fichero> Es verdadero si solo existe
--d <directorio> Es verdadero si es un directorio
--s <fichero> Es verdadero si existe y tiene un tamaño superior a 0
-'
-
 
 echo ""
 echo ""
